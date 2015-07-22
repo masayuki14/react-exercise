18f27049a7db1ada6a8dd2013f5d849f91797b87
Masayuki Morisaki <masayuki144@gmail.com>
2015-07-22 07:51:27 +0000

    first commit. Hello React by jsx build.

---
+++ dest/app.js
@@ -0,0 +1,9 @@
+var App = React.createClass({displayName: "App",
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement("p", null, "Hello, React")
+      )
+    );
+  }
+});
---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Hello, React</p>
+      </div>
+    );
+  }
+});
